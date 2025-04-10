d8d516b396a224d7773a096b44b296bc554b94fd
RhuanCarvalho <68081483+RhuanCarvalho@users.noreply.github.com>
2025-04-10 18:13:45 -0300

    ajustes no redirecionamento

--- src/app/budget/create/confirmSend/confirmSend.tsx
+++ src/app/budget/create/confirmSend/confirmSend.tsx
@@ -1,20 +1,25 @@
 import { useAllServices } from "@/services/allServices";
 
 interface ConfirmationSendBugdgetProps {
-    onClick?: () => void;
+    onClick: () => void;
 }
 
 
 export const ConfirmationSendBugdget = ({ onClick }: ConfirmationSendBugdgetProps) => {
     
-    const { state: { retornoMessageAPI }} = useAllServices();
+    const { state: { retornoMessageAPI }, actions: { cleanMessage }} = useAllServices();
+
+    const handleOnClick = () => {
+        onClick();
+        cleanMessage();
+    }
     
     return (
         <div className="w-full pt-36 flex flex-col items-center justify-center">
             <p className="text-2xl font-bold text-center">{!!retornoMessageAPI ? retornoMessageAPI : "Excelente, seu orçamento foi enviado com sucesso! Boa sorte!"}</p>
             <div className="w-full flex justify-center items-center pt-20">
                 <button
-                    onClick={onClick}
+                    onClick={handleOnClick}
                     className="transition-all w-[60%] bg-blue-500 rounded-full active:scale-105 active:bg-blue-600 hover:bg-blue-600 cursor-pointer text-white text-medium font-bold py-2 px-4"
                 >
                     Ver orçamentos
--- src/app/budget/create/messageDecline/messageDecline.tsx
+++ src/app/budget/create/messageDecline/messageDecline.tsx
@@ -1,19 +1,25 @@
 import { useAllServices } from "@/services/allServices";
 
 interface ConfirmDeclineBugdgetProps {
-    onClick?: () => void;
+    onClick: () => void;
 }
 
 
 export const ConfirmDeclineBugdget = ({ onClick }: ConfirmDeclineBugdgetProps) => {
 
-    const { state: { retornoMessageAPI }} = useAllServices();
+    const { state: { retornoMessageAPI }, actions: { cleanMessage }} = useAllServices();
+
+    const handleOnClick = () => {
+        onClick();
+        cleanMessage();
+    }
+
     return (
         <div className="w-full pt-36 flex flex-col items-center justify-center">
             <p className="text-2xl font-bold text-center">{!!retornoMessageAPI ? retornoMessageAPI : "Recebemos a sua confirmação, obrigado!"}</p>
             <div className="w-full flex justify-center items-center pt-20">
                 <button
-                    onClick={onClick}
+                    onClick={handleOnClick}
                     className="transition-all w-[60%] bg-blue-500 rounded-full active:scale-105 active:bg-blue-600 hover:bg-blue-600 cursor-pointer text-white text-medium font-bold py-2 px-4"
                 >
                     Ver outros orçamentos
