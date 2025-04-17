233d7ca71ee931df0cba0468179a4f6aa02e6398
RhuanCarvalho <68081483+RhuanCarvalho@users.noreply.github.com>
2025-04-17 19:09:18 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+const IS_DEV = process.env.NEXT_PUBLIC_IS_DEV === 'true';
+
+const basePath =  IS_DEV ? '' : "/painel/link"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: basePath,
+  trailingSlash: true, // opcional, mas ajuda com / no final das URLs
 };
 
 export default nextConfig;
